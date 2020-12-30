16be2f066db9d63473a790fc76c0929e05d15472
Tim Steeman <tmsteem82@gmail.com>
2020-12-30 15:21:05 +0100

    feature: implemented example sentence model, service, and endpoints ref: closes #8, closes #7, closes #9

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import kanji, compound_word
+from app.api.api_v1.endpoints import kanji, compound_word, example_sentence
 
 api_router = APIRouter()
 api_router.include_router(kanji.router, prefix="/kanjis", tags=["kanjis"])
 api_router.include_router(compound_word.router, prefix="/compound-words", tags=["compound words"])
+api_router.include_router(example_sentence.router, prefix="/example-sentences", tags=["example sentences"])
